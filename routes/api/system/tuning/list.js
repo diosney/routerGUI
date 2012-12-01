469cf3fc0cbdd43c570b3b298e49dd449f16e298
Diosney <diosney.s@gmail.com>
2012-11-30 23:35:05 -0500

    System/Tunables: Fixed grid listing code.

--- routes/api/system/tuning/list.js
+++ routes/api/system/tuning/list.js
@@ -5,6 +5,7 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+	async = require('async'),
 
 /*
  * Load required models.
@@ -21,37 +22,51 @@ module.exports = function (req, res) {
 			 * Returns a list of Tunables from database.
 			 */
 			// TODO: Add sorting functionality.
-			Tunable.find({}, {}, {
-				skip :req.query.page * req.query.rows - req.query.rows,
-				limit:req.query.rows
-			}, function (error, docs) {
-				if (!error) {
-					var count = docs.length;
+			async.waterfall([
+				function (callback_waterfall) {
+					// Obtain the total count of object in database.
+					Tunable.find({}, {}, {
+					}, function (error, docs) {
+						if (!error) {
+							callback_waterfall(null, docs.length);
+						}
+						else {
+							callback_waterfall(error);
+						}
+					});
+				}
+			], function (error, count) {
+				Tunable.find({}, {}, {
+					skip :req.query.page * req.query.rows - req.query.rows,
+					limit:req.query.rows,
+					sort :'group'
+				}, function (error, docs) {
+					if (!error) {
+						response_from_server.records = count;
+						response_from_server.page = req.query.page;
+						response_from_server.total = Math.ceil(count / req.query.rows);
+						response_from_server.rows = [];
 
-					response_from_server.records = count;
-					response_from_server.page = req.query.page;
-					response_from_server.total = Math.ceil(count / req.query.rows);
-					response_from_server.rows = [];
+						for (item in docs) {
+							response_from_server.rows.push({
+								id  :docs[item].id,
+								cell:[
+									docs[item].group,
+									docs[item].description,
+									docs[item].path,
+									docs[item].value
+								]
+							});
+						}
 
-					for (item in docs) {
-						response_from_server.rows.push({
-							id  :docs[item].id,
-							cell:[
-								docs[item].group,
-								docs[item].description,
-								docs[item].path,
-								docs[item].value
-							]
-						});
+						// Return the gathered data.
+						res.json(response_from_server);
 					}
-
-					// Return the gathered data.
-					res.json(response_from_server);
-				}
-				else {
-					// TODO: See how pass error message to grid list action and show it.
-					console.log('// ERROR: TODO: See how pass error message to grid list action and show it.');
-				}
+					else {
+						// TODO: See how pass error message to grid list action and show it.
+						console.log('// ERROR: TODO: See how pass error message to grid list action and show it.');
+					}
+				});
 			});
 
 			break;
